92eaafe9a34f5f89cc392f1919e79c6a7de6e333
mohamedsemah <medsemah@umich.edu>
2025-06-20 00:33:05 -0400

    Updated my TV shows dataset

--- src/data/tvShowsData.js
+++ src/data/tvShowsData.js
@@ -595,7 +595,7 @@ export const tvShowsData = [
   },
   {
     question: "Which Sylvester Stallone series follows a New York mobster building a criminal empire in Oklahoma?",
-    answer: "Tulsa King",
+    answer: "Tulsa King.",
     details: "Stallone plays Dwight 'The General' Manfredi in this Paramount+ crime drama",
     image: "https://wwwimage-us.pplusstatic.com/base/files/seo/66/92/74/7d2721e6-f5db-4e0d-b50f-22cf608e5813.jpg?format=webp"
   },
